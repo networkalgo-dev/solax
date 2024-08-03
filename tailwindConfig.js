647b6e486e09adc741a5ee59f1faf2bd7a196462
networkalgo-dev <networkalgo24@gmail.com>
2024-08-03 13:39:17 +0530

    successStories page added

---
+++ tailwindConfig.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      primary: ["Jost", "sans-serif"],
+      secondary: ["Satoshi", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "992px",
+        xl: "1280px",
+        "2xl": "1440px",
+        "3xl": "1920px",
+      },
+      colors: {
+        primary: {
+          black: "#151515",
+          white: "#F5F5F5",
+          grey: "#C8C8C8",
+          yellow: "#EEBB0C",
+          orange: "#EF6818",
+        },
+      },
+    },
+  },
+};
